5241e0f4b6db3642d4553fc2ac2bdd1981cc581e
James Robinson <jrstats@outlook.com>
2021-05-26 23:14:57 +0100

    change widget to use cron and priority

--- classWidget.py
+++ classWidget.py
@@ -2,9 +2,11 @@ import datetime
 import logging
 
 class Widget():
-    def __init__(self, widgetName, refreshRate):
+    def __init__(self, widgetName, cronSyntax, priority, params):
         self.widgetName = widgetName
-        self.refreshRate = refreshRate
+        self.cronSyntax = cronSyntax
+        self.priority = priority
+        self.params = params
 
     def update(self):
         logging.info(f"updated widget {self.widgetName} at: {datetime.datetime.now()}")
