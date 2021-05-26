dd0c5e33e649bce5eec6d66b537286ea71d53649
James Robinson <jrstats@outlook.com>
2021-05-26 23:28:23 +0100

    start looking at national rail API

--- proofs/proofRail.py
+++ proofs/proofRail.py
@@ -1,1 +1,8 @@
 # https://github.com/nwootton/MMM-UKNationalRail
+# https://www.nationalrail.co.uk/100296.aspx
+# http://lite.realtime.nationalrail.co.uk/openldbws/
+# https://www.nationalrail.co.uk/stations_destinations/48541.aspx
+
+# SOAP web service
+
+token = "fdbdc556-f184-414a-ae83-b98d6ae2dcf0"
