0e9a553cb870de94ca2ac1654ca0dd4e01fc5f12
James Robinson <jrstats@outlook.com>
2021-05-31 23:43:33 +0100

    add section to tkinter proof

--- proofs/proofTkinter.py
+++ proofs/proofTkinter.py
@@ -1,25 +1,40 @@
-from tkinter import *
+import tkinter as tk
 import datetime
 
  
-root = Tk()
-root.geometry("1440x900+0+0")
+# root = Tk()
+# root.geometry("1440x900+0+0")
  
-frame = Frame(root)
-frame.pack()
+# frame = Frame(root)
+# frame.pack()
  
-now = datetime.datetime.now()
-time = now.strftime("%H:%M:%S")
+# now = datetime.datetime.now()
+# time = now.strftime("%H:%M:%S")
 
-label = Label(frame, text=time)
-label.pack()
+# label = Label(frame, text=time)
+# label.pack()
 
  
+# # root.mainloop()
+
+# while True:
+#     now = datetime.datetime.now()
+#     time = now.strftime("%H:%M:%S.%f")
+#     label.config(text=time)
+#     label.update()
+#     print(now, "updating")
+
+# root = tk.Tk()
+# root.geometry("600x600")
+# pane = tk.Frame(root, bg="#000000", width=600, height=600)
+# pane.pack(side="left", fill="x")
+
+# output = ("abc", "def")
+# time_label = tk.Label(pane, text=output[0], fg="white", bg="#000000")
+# date_label = tk.Label(pane, text=output[1], fg="white", bg="#000000")
+# time_label.grid(row=1,sticky="nw")
+# date_label.grid(row=2,sticky="nw")
+
+# pane.grid_propagate(0)
+
 # root.mainloop()
-
-while True:
-    now = datetime.datetime.now()
-    time = now.strftime("%H:%M:%S.%f")
-    label.config(text=time)
-    label.update()
-    print(now, "updating")
