3f2164052d0b8a8d4b7d96b718920b3dddc4c7f5
jrstats <jrstats@outlook.com>
2021-06-04 18:49:23 +0100

    rename config to settings

--- classes/classConfig.py
+++
@@ -1,6 +0,0 @@
-
-
-
-class Config():
-    def __init__(self, configPath: str="../config-default.json"):
-        pass
---
+++ classes/classSettings.py
@@ -0,0 +1,6 @@
+
+
+
+class Settings():
+    def __init__(self, settingsPath: str="../config-default.json"):
+        self.settingsPath: str = settingsPath
