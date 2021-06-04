56511d6ca2221e56e001c480174306ad8f2a14d0
jrstats <jrstats@outlook.com>
2021-06-04 18:49:07 +0100

    test RSS feed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 from classes.classWidgetClock import WidgetClock
+from classes.classWidgetRss import WidgetRss
 from classes.classWindow import Window
 from classes.classOrchestrator import Orchestrator
 
@@ -21,21 +22,21 @@ if __name__ == "__main__":
             "is_multi":1},
         "displayNumberOfItems": 3}
 
-    w1 = WidgetClock(
-        widgetName="widget1", 
-        cronSyntax="* * * * *", 
+    wc = WidgetClock(
+        widgetName="clock", 
+        cronSyntax="* * * * * *", 
         priority=1, 
         config=configClock,
         pane="left",
         slotNumber=0)
-    w2 = WidgetClock(
-        widgetName="widget2", 
-        cronSyntax="* * * * *", 
+    wr = WidgetRss(
+        widgetName="rssFeed", 
+        cronSyntax="0 * * * * *", 
         priority=1, 
-        config=configClock,
+        config=configRss,
         pane="right",
         slotNumber=0)
     window = Window()
 
-    o = Orchestrator([w1, w2], window)
+    o = Orchestrator([wc, wr], window)
     o.live()
