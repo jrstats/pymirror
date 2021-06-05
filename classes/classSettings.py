1747a2cb0bccb10090c6ccc5ccd81bc595a742d1
jrstats <jrstats@outlook.com>
2021-06-05 19:15:09 +0100

    add to classSettings

--- classes/classSettings.py
+++ classes/classSettings.py
@@ -1,6 +1,34 @@
-
+from .keys import weatherKey
 
 
 class Settings():
-    def __init__(self, settingsPath: str="../config-default.json"):
-        self.settingsPath: str = settingsPath
+    WEATHER = {
+        "api_key": weatherKey,
+        "base_url": "https://api.openweathermap.org/data/2.5/onecall?",
+        "lat": "51.5149",
+        "lon": "0.3019",
+        "exclude": "minutely,hourly",
+        "units": "metric",
+        "desired_fields": ["dt", "feels_like", "weather"]
+    }
+
+    CLOCK = {
+        "dateFormat": "%Y-%m-%d",
+        "timeFormat": "%H:%M:%S"
+    }
+
+    BINS = {
+        "baseUrl": "https://www.ealing.gov.uk/site/custom_scripts/waste_collection/waste_collection.aspx",
+        "postCode": "W5 2AR",
+        "binsOfInterest": ["BLUE RECYCLING WHEELIE BIN", "FOOD BOX", "BLACK RUBBISH WHEELIE BIN"]
+    }
+
+    RSS = {
+        "baseUrl": "https://www.reddit.com/user/m-xames/m/uk_politics/search.rss?",
+        "searchTerms": {
+            "q": "is_self:0 NOT site:(500px.com OR abload.de OR deviantart.com OR deviantart.net OR fav.me OR fbcdn.net OR flickr.com OR forgifs.com OR giphy.com OR gfycat.com OR gifsoup.com OR gyazo.com OR imageshack.us OR imgclean.com OR imgur.com OR instagr.am OR instagram.com OR mediacru.sh OR media.tumblr.com OR min.us OR minus.com OR myimghost.com OR photobucket.com OR picsarus.com OR puu.sh OR i.redd.it OR staticflickr.com OR tinypic.com OR twitpic.com)",
+            "sort": "hot",
+            "restrict_sr":1,
+            "is_multi":1},
+        "displayNumberOfItems": 3
+    }
--- main.py
+++ main.py
@@ -1,49 +1,33 @@
 import logging
 
-from classes import Orchestrator, Window
+from classes import Orchestrator, Settings, Window
 from classes import WidgetClock, WidgetBins, WidgetRss
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="%(asctime)s:%(levelname)s:%(message)s")
-    configClock = {
-        "dateFormat": "%Y-%m-%d",
-        "timeFormat": "%H:%M:%S"}
-    configBins = {
-        "baseUrl": "https://www.ealing.gov.uk/site/custom_scripts/waste_collection/waste_collection.aspx",
-        "postCode": "W5 2AR",
-        "binsOfInterest": ["BLUE RECYCLING WHEELIE BIN", "FOOD BOX", "BLACK RUBBISH WHEELIE BIN"]}
-    configRss = {
-        "baseUrl": "https://www.reddit.com/user/m-xames/m/uk_politics/search.rss?",
-        "searchTerms": {
-            "q": "is_self:0 NOT site:(500px.com OR abload.de OR deviantart.com OR deviantart.net OR fav.me OR fbcdn.net OR flickr.com OR forgifs.com OR giphy.com OR gfycat.com OR gifsoup.com OR gyazo.com OR imageshack.us OR imgclean.com OR imgur.com OR instagr.am OR instagram.com OR mediacru.sh OR media.tumblr.com OR min.us OR minus.com OR myimghost.com OR photobucket.com OR picsarus.com OR puu.sh OR i.redd.it OR staticflickr.com OR tinypic.com OR twitpic.com)",
-            "sort": "hot",
-            "restrict_sr":1,
-            "is_multi":1},
-        "displayNumberOfItems": 3}
-
     wc = WidgetClock(
         widgetName="clock", 
         cronSyntax="* * * * * * *", 
         priority=1, 
-        config=configClock,
+        config=Settings.CLOCK,
         pane="left",
         slotNumber=0)
     wr = WidgetRss(
         widgetName="rssFeed", 
         cronSyntax="0 * * * * * *", 
         priority=1, 
-        config=configRss,
+        config=Settings.RSS,
         pane="right",
         slotNumber=0)
-    wb = WidgetBins(
-        widgetName="bins",
-        cronSyntax="0 0 * * * * *",
-        priority=1,
-        config=configBins,
-        pane="right",
-        slotNumber=1
-    )
+    # wb = WidgetBins(
+    #     widgetName="bins",
+    #     cronSyntax="0 0 * * * * *",
+    #     priority=1,
+    #     config=Settings.BINS,
+    #     pane="right",
+    #     slotNumber=1
+    # )
     window = Window()
 
-    o = Orchestrator([wc, wr, wb], window)
+    o = Orchestrator([wc, wr], window)
     o.live()
